64329f32f11b2535c40dc90a2ae989c77dbc1958
Kamil Jozwik <kamil.jozwik@britishcouncil.org>
2019-08-09 10:17:18 +0200

    typescript working

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    'gatsby-plugin-tslint',
     'gatsby-plugin-typescript',
     {
       resolve: `gatsby-source-filesystem`,
