69807889198d45d30851b34a1752d4c7a337133e
Kamil Jozwik <kamiljozwikpl@gmail.com>
2019-09-22 10:49:59 +0200

    categories names

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -13,9 +13,72 @@ export const categoriesNames = {
     name: 'JavaScript',
     subcategories: {
       animation: 'Animation',
+      authorization: 'Authorization',
+      cheatsheets: 'Cheatsheets',
+      data_layer: 'Data layer',
+      data_visualization: 'Data visualization',
+      extensions: 'Extensions',
+      frameworks: 'Frameworks',
+      graphic: 'Graphic',
+      interactions: 'Interactions',
+      linters: 'Linters',
+      package_managers: 'Package managers',
+      search: 'Searching',
+      tests: 'Tests',
+      translations: 'Translations',
+      utils: 'Utilities',
     },
   },
   css: {
     name: 'CSS',
+    subcategories: {
+      animations: 'Animations',
+      cheatsheets: 'Cheatsheets',
+      css_in_js: 'CSS in JS',
+      frameworks: 'Frameworks',
+      processors: 'Procesors',
+      utils: 'Utilities',
+    },
+  },
+  seo: {
+    name: 'SEO',
+    subcategories: {
+      empty: 'empty',
+    },
+  },
+  ide: {
+    name: 'IDE',
+    subcategories: {
+      empty: 'empty',
+    },
+  },
+  build: {
+    name: 'Build',
+    subcategories: {
+      empty: 'empty',
+    },
+  },
+  task: {
+    name: 'Task',
+    subcategories: {
+      empty: 'empty',
+    },
+  },
+  monitor: {
+    name: 'Monitor',
+    subcategories: {
+      empty: 'empty',
+    },
+  },
+  ux: {
+    name: 'UX',
+    subcategories: {
+      colors: 'Colors',
+      design: 'Design',
+      fonts: 'Fonts',
+      graphics: 'Graphics',
+      icons: 'Icons',
+      logos: 'Logos',
+    },
   },
 };
