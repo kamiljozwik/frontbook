22cc351eaebc3f74621cb60f59c30565b0d74a32
Kamil Jozwik <kamiljozwikpl@gmail.com>
2019-11-24 17:31:46 +0100

    svg names change

--- src/components/SVG/CategoryWave.tsx
+++ src/components/SVG/CategoryWave.tsx
@@ -1,10 +1,8 @@
 import React, { SVGAttributes } from 'react';
 import styled from '@emotion/styled';
 
-import { mq } from '../layout';
-
 export const CategoryWave = (props: SVGAttributes<Element>) => (
-  <StyledSVG viewBox="0 0 1920 316" fill="none" {...props}>
+  <StyledCategorySVG viewBox="0 0 1920 316" fill="none" {...props}>
     <path d="M0 282.5S253.56 316 398 316c356.584 0 720.5-56 989-56s533 42 533-49.5V0H0v282.5z" fill={props.color} />
     <path
       d="M0 282.5S253.56 316 398 316c356.584 0 720.5-56 989-56s533 42 533-49.5V0H0v282.5z"
@@ -23,15 +21,13 @@ export const CategoryWave = (props: SVGAttributes<Element>) => (
         <stop offset={1} stopColor="#fff" stopOpacity={0.59} />
       </linearGradient>
     </defs>
-  </StyledSVG>
+  </StyledCategorySVG>
 );
 
-const StyledSVG = styled.svg`
+const StyledCategorySVG = styled.svg`
   position: absolute;
   top: 0;
   left: 0;
   z-index: -1;
-  ${mq({
-    minHeight: ['60vh', '40vh', '40vh', '40vh', '40vh'],
-  })}
+  min-height: 40vh;
 `;
--- src/components/SVG/NpmLogo.tsx
+++ src/components/SVG/NpmLogo.tsx
@@ -2,7 +2,7 @@ import React, { SVGAttributes } from 'react';
 import styled from '@emotion/styled';
 
 export const NpmLogo = (props: SVGAttributes<Element>) => (
-  <StyledSVG width="36px" viewBox="0 0 18 7" {...props}>
+  <StyledNpmSVG width="36px" viewBox="0 0 18 7" {...props}>
     <path
       fill="#CB3837"
       d="M0 0h18v6H9v1H5V6H0V0zm1 5h2V2h1v3h1V1H1v4zm5-4v5h2V5h2V1H6zm2 1h1v2H8V2zm3-1v4h2V2h1v3h1V2h1v3h1V1h-6z"
@@ -10,10 +10,10 @@ export const NpmLogo = (props: SVGAttributes<Element>) => (
     <path fill="#FFF" d="M1 5L3 5 3 2 4 2 4 5 5 5 5 1 1 1z" />
     <path fill="#FFF" d="M6 1v5h2V5h2V1H6zm3 3H8V2h1v2z" />
     <path fill="#FFF" d="M11 1L11 5 13 5 13 2 14 2 14 5 15 5 15 2 16 2 16 5 17 5 17 1z" />
-  </StyledSVG>
+  </StyledNpmSVG>
 );
 
-const StyledSVG = styled.svg`
+const StyledNpmSVG = styled.svg`
   position: relative;
   top: 4px;
 `;
